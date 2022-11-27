f0b4059b2394d49386901436705874b57ac3fd32
unknown <alsabaaferas@gmail.com>
2022-11-27 10:55:44 +0100

    add example

---
+++ hello-world/npm_tese/react-demo/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import CustomBtn from './Components/CustomBtn';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomBtn/>
+      <h1> Hellooooo Matte</h1>
+    </div>
+  );
+}
+
+export default App;
