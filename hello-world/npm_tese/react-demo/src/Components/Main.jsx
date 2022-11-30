48b3c9e21028eb279735f87e764cc592a2d4e3dc
unknown <alsabaaferas@gmail.com>
2022-11-30 10:09:34 +0100

    add pic to the video

--- hello-world/npm_tese/react-demo/src/Components/Main.jsx
+++ hello-world/npm_tese/react-demo/src/Components/Main.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import videoBg from '../assests/video_m1.mp4'
+import videoBg from '../assests/videos/video_m1.mp4'
+import img_1 from '../assests/images/img_1.png'
 
 export default function Main() {
   return (
     <div className='main'>
-        <div className='overlay'></div>
-        <video src={videoBg} autoPlay></video>
-        <div className='content'>
-            <h1>Welcome</h1>
-            <p>to my site Matte.</p>
+        <div className='overlay'>
+         <video src={videoBg} autoPlay></video>
+                <div className='content'>
+                  <img src={img_1}></img>
+                </div>
         </div>
     </div>
   )
