baca30f36592c85913c3b61ccdc332a8bc32e949
Fawaz Bechara <fawaz.bechara@gmail.com>
2022-12-28 12:24:26 +0100

    Small changes

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("tutorials_home.html")
+
+@app.route("/tutorial1")
+def tutorial1():
+    return render_template("hello-world.html")
+
+@app.route("/tutorial2")
+def tutorial2():
+    return render_template("rc-interaction.html")
+
+@app.route("/tutorial3")
+def tutorial3():
+    return render_template("broadcast-object.html")
