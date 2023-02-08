a4a492ca9a5c9490fa1d3254c0d4ca12a3703225
unknown <alsabaaferas@gmail.com>
2023-02-08 14:03:57 +0100

    push changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
@@ -11,9 +11,51 @@ def load_json():
     f.close()
     return json_data
 
+def load_json_ads():
+    f = open("ads.json")
+    json_data_ads = json.load(f)
+    print(json_data_ads)
+    f.close()
+    return json_data_ads
+
 @app.route("/")
 def home():
     return render_template("tutorials_home.html")
+
+@app.route('/ads/<string:name>')
+def get_ads(name):
+    json_data_ads = load_json_ads()
+    for ad in json_data_ads:
+        print('+++')
+        print(ad['type'])
+        if(ad['type']==name):
+            return jsonify(ad['type'])
+    return jsonify({'message':'Ad not found'})
+
+@app.route('/ad',methods=['POST'])
+def create_ad():
+    json_data_ads = load_json_ads()
+    req_data = request.get_json()
+    new_ad={
+        'type': req_data['type'],
+        'width': req_data['width']
+    }
+    json_data_ads.append(new_ad)
+    return jsonify(new_ad)
+
+@app.route('/ad/<string:type>/<string:width>',methods=['POST'])
+def create_ad_item():
+    json_data_ads = load_json_ads()
+    for ad in json_data_ads:
+        if(ad['type']==type):
+            req_data = request.get_json()
+            new_item = {
+                'type':req_data['type'],
+                'width':req_data['width']
+            }
+            json_data_ads['type'].append(new_item)
+            return jsonify(new_item)
+    return jsonify({'message':'Ad not found'})
 
 @app.route("/tutorial1")
 def tutorial1():
