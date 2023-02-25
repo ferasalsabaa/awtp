1ecf1f943f322c9d0e3e81159d4c293b84455f4a
unknown <alsabaaferas@gmail.com>
2023-02-25 17:02:32 +0100

    add convert json to vast xml

--- webserver/main.py
+++ webserver/main.py
@@ -52,5 +52,74 @@ def send_file():
         return json_object
     return 'No'
 
+@app.route('/json-to-vast', methods=['POST'])
+def json_to_vast():
+    json_data = request.get_json()
+
+    vast_xml = create_vast_xml(json_data)
+
+    response = Response(vast_xml, content_type='application/xml')
+
+    return response
+
+def create_vast_xml(json_data):
+    width = json_data['generalInfo']["width"]
+    type_ad = json_data['generalInfo']["type_ad"]
+    link_url = json_data['generalInfo']["link-url"]
+    interactivity = json_data['generalInfo']["interactivity"]
+    height = json_data['generalInfo']["height"]
+    duration = json_data['generalInfo']["duration"]
+    background_type = json_data['generalInfo']["background-type"]
+    background_color = json_data['generalInfo']["background-color"]
+
+    alignment_left = json_data['generalInfo']["alignment"]["left"]
+    alignment_top = json_data['generalInfo']["alignment"]["top"]
+
+
+    ad_id = json_data['ad_id']
+    ad_title = json_data['ad_title']
+    ad_description = json_data['ad_description']
+    ad_url = json_data['ad_url']
+    impression_url = json_data['impression_url']
+    clickthrough_url = json_data['clickthrough_url']
+
+    # Create VAST XML document using extracted data
+    vast_xml = f'''<VAST version="4.2">
+        <Ad id="{ad_id}">
+            <InLine>
+                <AdSystem>Example Ad System</AdSystem>
+                <AdTitle>{ad_title}</AdTitle>
+                <Description>{ad_description}</Description>
+                <Impression>
+                    <URL>{impression_url}</URL>
+                </Impression>
+                <Creatives>
+                    <Creative>
+                        <Linear>
+                            <VideoClicks>
+                                <ClickThrough>{clickthrough_url}</ClickThrough>
+                            </VideoClicks>
+                        </Linear>
+                    </Creative>
+                </Creatives>
+                <generalInfo>
+                    <alignment>
+                        <left>{alignment_left}</left>
+                        <top>{alignment_top}</top>
+                    </alignment>
+                    <background-color>{background_color}</background-color>
+                    <background-type>{background_type}</background-type>
+                    <duration>{duration}</duration>
+                    <height>{height}</height>
+                    <interactivity>{interactivity}</interactivity>
+                    <link-url>{link_url}</link-url>
+                    <type>{type_ad}</type>
+                    <width>{width}</width>
+                </generalInfo>
+            </InLine>
+        </Ad>
+    </VAST>'''
+    return vast_xml
+
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
